c7995ccecb6cdfc465f94a57b115fbcc9e1ccf75
Munazirraza <munazirraza2315@gmail.com>
2023-09-30 23:50:04 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import Modal from "./Modal";
+
 
 const App = () => {
+
+  let[show,setShow]=useState(false)
+
+  const displayModal=()=>{
+
+    setShow(true)
+  }
+
+
+  const onClose=()=>{
+
+    setShow(false)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <button onClick={displayModal}>Show Modal</button>
+        { show ?
+        <Modal show={show} onClose={onClose}/>
+        : ""
+        }
     </div>
   )
 }
